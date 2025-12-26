aade504b72a0bc6fdf2967acd168f09191a8430d
Marco Marques <marco.marques@popcode.com.br>
2025-12-26 15:45:58 -0300

    Estrutura de Tokens.

---
+++ Sources/HandzDesignSystem/Tokens/Colors/DSColors.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  HandzDesignSystem
+//
+//  Created by Marco Henrique on 26/12/25.
+//
+
---
+++ Sources/HandzDesignSystem/Tokens/Radius/DSRadius.swift
@@ -0,0 +1,7 @@
+//
+//  DSRadius.swift
+//  HandzDesignSystem
+//
+//  Created by Marco Henrique on 26/12/25.
+//
+
